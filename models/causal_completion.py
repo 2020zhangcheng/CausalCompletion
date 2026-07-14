33dce6df56520d394b7e703fe33c861ae4697aec
anonymous <62683269+2020zhangcheng@users.noreply.github.com>
2026-07-14 22:33:46 +0800

    Update causal_completion.py

--- models/causal_completion.py
+++ models/causal_completion.py
@@ -4,61 +4,11 @@ from torch import nn
 from pointnet2_ops import pointnet2_utils
 from extensions.chamfer_dist import ChamferDistanceL1
 from .Transformer import PCTransformer
+from .PoinTr import Fold, fps
 from .build import MODELS
 
-
-def fps(pc, num):
-    fps_idx = pointnet2_utils.furthest_point_sample(pc, num) 
-    sub_pc = pointnet2_utils.gather_operation(pc.transpose(1, 2).contiguous(), fps_idx).transpose(1,2).contiguous()
-    return sub_pc
-
-
-class Fold(nn.Module):
-    def __init__(self, in_channel , step , hidden_dim = 512):
-        super().__init__()
-
-        self.in_channel = in_channel
-        self.step = step
-
-        a = torch.linspace(-1., 1., steps=step, dtype=torch.float).view(1, step).expand(step, step).reshape(1, -1)
-        b = torch.linspace(-1., 1., steps=step, dtype=torch.float).view(step, 1).expand(step, step).reshape(1, -1)
-        self.folding_seed = torch.cat([a, b], dim=0).cuda()
-
-        self.folding1 = nn.Sequential(
-            nn.Conv1d(in_channel + 2, hidden_dim, 1),
-            nn.BatchNorm1d(hidden_dim),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(hidden_dim, hidden_dim//2, 1),
-            nn.BatchNorm1d(hidden_dim//2),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(hidden_dim//2, 3, 1),
-        )
-
-        self.folding2 = nn.Sequential(
-            nn.Conv1d(in_channel + 3, hidden_dim, 1),
-            nn.BatchNorm1d(hidden_dim),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(hidden_dim, hidden_dim//2, 1),
-            nn.BatchNorm1d(hidden_dim//2),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(hidden_dim//2, 3, 1),
-        )
-
-    def forward(self, x):
-        num_sample = self.step * self.step
-        bs = x.size(0)
-        features = x.view(bs, self.in_channel, 1).expand(bs, self.in_channel, num_sample)
-        seed = self.folding_seed.view(1, 2, num_sample).expand(bs, 2, num_sample).to(x.device)
-
-        x = torch.cat([seed, features], dim=1)
-        fd1 = self.folding1(x)
-        x = torch.cat([fd1, features], dim=1)
-        fd2 = self.folding2(x)
-
-        return fd2
-
 @MODELS.register_module()
-class PoinTr(nn.Module):
+class CausalCompletion(nn.Module):
     def __init__(self, config, **kwargs):
         super().__init__()
         self.trans_dim = config.trans_dim
@@ -102,16 +52,9 @@ class PoinTr(nn.Module):
             coarse_point_cloud], dim=-1)  # B M 1027 + C
 
         rebuild_feature = self.reduce_map(rebuild_feature.reshape(B*M, -1)) # BM C
-        # # NOTE: try to rebuild pc
-        # coarse_point_cloud = self.refine_coarse(rebuild_feature).reshape(B, M, 3)
 
-        # NOTE: foldingNet
         relative_xyz = self.foldingnet(rebuild_feature).reshape(B, M, 3, -1)    # B M 3 S
         rebuild_points = (relative_xyz + coarse_point_cloud.unsqueeze(-1)).transpose(2,3).reshape(B, -1, 3)  # B N 3
-
-        # NOTE: fc
-        # relative_xyz = self.refine(rebuild_feature)  # BM 3S
-        # rebuild_points = (relative_xyz.reshape(B,M,3,-1) + coarse_point_cloud.unsqueeze(-1)).transpose(2,3).reshape(B, -1, 3)
 
         # cat the input
         inp_sparse = fps(xyz, self.num_query)
